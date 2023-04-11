a881f76044d07e46dd3d4de80cfcf6f34f952dcf
Patrick-Cabelin <108542359+Patrick-Cabelin@users.noreply.github.com>
2023-04-11 20:17:52 -0300

    #

--- src/server.js
+++ src/server.js
@@ -2,12 +2,17 @@ require('express-async-errors')
 const MigrationsRun = require('./database/sqlite/migrations')
 MigrationsRun()
 
+const cors = require('cors')
 const express = require('express')
 app = express()
 app.use(express.json())
+app.use(cors())
 
 const routes = require('./routes')
 app.use(routes)
+
+const upload = require('./config/upload')
+app.use('/files', express.static(upload.UPLOAD_FOLDER)) 
 
 const AppError = require('./utils/AppError')
 
