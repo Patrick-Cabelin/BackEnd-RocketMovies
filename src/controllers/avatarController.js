20f0989280da710c9d2de003e810be14586fca2a
Patrick-Cabelin <108542359+Patrick-Cabelin@users.noreply.github.com>
2023-04-11 21:15:59 -0300

    Avatar controller

---
+++ src/controllers/avatarController.js
@@ -0,0 +1,26 @@
+const knex = require('knex')
+const AppError = require('../utils/AppError')
+const DiskStorage = require('../providers/diskStorage')
+
+class avatarController{
+    async Update(request , response){
+        const diskstorage = new DiskStorage()
+        
+        const user_id = request.user.id
+        const avatarFileName = request.file.filename
+
+        const user = await knex('user').where({id : user_id}).first()
+        if(!user) throw AppError('Ação não autorizada',401)
+
+        if (user.avatar) await diskstorage.deleteFile(user.avatar)
+
+        const filename = await diskstorage.saveFile(avatarFileName)
+        user.avatar = filename
+         
+        await knex('users').update(user).where({id : user})
+
+        return response.json(user)
+    }
+}
+
+module.exports = avatarController
---
+++ src/providers/diskStorage.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const uploadConfig = require('../config/upload')
+const fs = require('fs')
+
+class DiskStorage{
+    async SaveFile(file){
+        await fs.promises.rename(
+            path.resolve(uploadConfig.TMP_FOLDER, file),
+            path.resolve(uploadConfig.UPLOAD_FOLDER, file)
+        )
+
+        return file
+    }
+
+    async DeleteFile(file){
+        const path = path.resolve(uploadConfig.TMP_FOLDER, file)
+
+        try {
+            await fs.promises.stat(path)
+        } catch {
+            return
+        }
+
+        await fs.promises.unlink(path)
+    }
+}
+
+module.exports = DiskStorage
